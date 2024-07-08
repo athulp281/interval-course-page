48ef25573ee3fa5148374d44a150f19e74537b9c
athul <athulpranoor@gmail.com>
2024-07-08 16:00:39 +0530

    loader update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,50 +1,50 @@
-import React from "react";
-import "../utils/css/loader.css";
+// import React from "react";
+// import "../utils/css/loader.css";
 
-const Loader3D = React.memo(() => {
-    return (
-        <div className="loader-container-main">
-            <div className="loader-3d-animation">
-                {/* Use your icon as the background image */}
-                <div
-                    className="face front"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-                <div
-                    className="face back"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-                <div
-                    className="face left"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-                <div
-                    className="face right"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-                <div
-                    className="face top"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-                <div
-                    className="face bottom"
-                    style={{
-                        backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
-                    }}
-                ></div>
-            </div>
-        </div>
-    );
-});
+// const Loader3D = React.memo(() => {
+//     return (
+//         <div className="loader-container-main">
+//             <div className="loader-3d-animation">
+//                 {/* Use your icon as the background image */}
+//                 <div
+//                     className="face front"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//                 <div
+//                     className="face back"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//                 <div
+//                     className="face left"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//                 <div
+//                     className="face right"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//                 <div
+//                     className="face top"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//                 <div
+//                     className="face bottom"
+//                     style={{
+//                         backgroundImage: `url(https://www.teaminterval.ae/wp-content/uploads/2022/03/interval-logo.png)`,
+//                     }}
+//                 ></div>
+//             </div>
+//         </div>
+//     );
+// });
 
-export default Loader3D;
+// export default Loader3D;
