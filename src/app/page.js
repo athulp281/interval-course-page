53b5f9b1718c98165af7f170cf14130060539360
athul <athulpranoor@gmail.com>
2024-07-08 16:29:50 +0530

    update111

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-"use client";
 import ArticleSection from "@/sections/ArticleSection/ArticleSection";
 import FeaturedCourse from "@/sections/FeaturedCourse/FeaturedCourse";
 import FindMoreCourse from "@/sections/FindMoreCourse/FindMoreCourse";
@@ -8,54 +7,19 @@ import NavBar from "@/sections/NavBar/NavBar";
 import QuickGuidSection from "@/sections/QuickGuid/QuickGuidSection";
 import ReviewSection from "@/sections/ReviewSection/ReviewSection";
 import TopCourses from "@/sections/TopCourses/TopCourses";
-import useScrollReveal from "../hooks/useScrollReveal";
 
 export default function Home() {
-    useScrollReveal();
-
     return (
         <>
             <NavBar />
-            <div className="reveal">
-                <HeroSections />
-            </div>
-            <div className="reveal">
-                <TopCourses />
-            </div>
-            <div className="reveal">
-                <FeaturedCourse />
-            </div>
-            <div className="reveal">
-                <QuickGuidSection />
-            </div>
-            <div className="reveal">
-                <ReviewSection />
-            </div>
-            <div className="reveal">
-                <FindMoreCourse />
-            </div>
-            <div className="reveal">
-                <ArticleSection />
-            </div>
-            <Footer />
-
-            {/* <NavBar />
-
             <HeroSections />
-
             <TopCourses />
-
             <FeaturedCourse />
-
             <QuickGuidSection />
-
             <ReviewSection />
-
             <FindMoreCourse />
-
             <ArticleSection />
-
-            <Footer /> */}
+            <Footer />
         </>
     );
 }
